db62ffe8d2e098dd6ca3d709e2df5bee9ba95681
Loyle <antoine.charmeau@laposte.net>
2019-05-24 08:20:40 +0200

    Restart

--- eventDetector.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by marco on 21/05/2019.
-//
-
-#ifndef DAMASS_EVENTDETECTOR_H
-#define DAMASS_EVENTDETECTOR_H
-
-void eventDetector(SDL_Window*,SDL_Renderer*);
-#endif //DAMASS_EVENTDETECTOR_H
