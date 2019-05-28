5a5d3e1167ac7f71f8719af0d28527c00d1c2657
Loyle <antoine.charmeau@laposte.net>
2019-05-28 17:27:24 +0200

    Correction d'un bug

--- draw/initGameWindows.c
+++ draw/initGameWindows.c
@@ -1,12 +1,13 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include "draw.h"
+#include "../board/cellStructure.h"
 
 //
 // Created by Antoine on 28/05/2019.
 //
 
-void initGameWindows(SDL_Renderer* renderer, Cells **board) {
+void initGameWindows(SDL_Renderer* renderer, Cell **board) {
     SDL_SetRenderDrawColor(renderer, 208, 208, 208, 255);
     SDL_RenderClear(renderer);
 
